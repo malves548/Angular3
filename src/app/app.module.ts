b0297f4052cff3d02d9daa186f0101d57049cac0
Marcello <malves548@hotmail.com>
2020-05-04 06:18:42 -0300

    save pra n perder

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PessoasComponent } from './pessoas/pessoas.component';
+import { PessoasDetalheComponent } from './pessoas-detalhe/pessoas-detalhe.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PessoasComponent,
+    PessoasDetalheComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
